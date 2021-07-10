d9155bcfd88f4aa39e71e4e0cac4f20413f0874e
tu_usuario_de_GITHUB <tu_correo_de_GITHUB>
2021-07-10 14:57:12 -0500

    Se agregan las clases producto y app

---
+++ src/Producto.java
@@ -0,0 +1,11 @@
+public class Producto {
+//atributos
+    private String nombre = "";
+    private float precio = 0;
+//metodo
+    public void mostrar() {
+        System.out.println(nombre + ":" + precio);
+    }
+ 
+    
+}
