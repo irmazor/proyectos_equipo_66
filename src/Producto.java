ca8a16423fa94c6e189cbb655296bd8c701574b4
tu_usuario_de_GITHUB <tu_correo_de_GITHUB>
2021-07-11 15:18:08 -0500

    Se crea clase Producto con metodos accesorios y su respectivo objeto

--- src/Producto.java
+++ src/Producto.java
@@ -1,9 +1,32 @@
 public class Producto {
-//atributos
+    //ATRIBUTOS
     private String nombre = "";
     private float precio = 0;
-//metodo
+
+    //METODO
+
     public void mostrar() {
-        System.out.println(nombre + ":" + precio);
-    }   
+        String precioTexto = String.format("%,d", (int)precio);
+        System.out.println(nombre + ": " + precio);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return this.nombre;
 }
+
+public float getPrecio() {
+    return this.precio;
+}
+
+}
+
+
+
